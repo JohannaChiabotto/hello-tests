ab24aecfdfd78d7407e013759172102d891d8aa6
johannachiabotto <johanna.chiabotto@gmail.com>
2022-10-26 18:05:46 +0200

    update sout

--- src/main/java/org/example/BooleanSmallerThanOne.java
+++ src/main/java/org/example/BooleanSmallerThanOne.java
@@ -3,7 +3,7 @@ package org.example;
 public class BooleanSmallerThanOne {
     public static boolean booleanSmallerThanOne(float value) {
         if (value < 1) {
-            System.out.println("kleiner als 1");
+            System.out.println("kleiner kttrststrsfals 1");
             return true;
         }
         return false;
